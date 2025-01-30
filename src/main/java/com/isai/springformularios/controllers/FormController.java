88bbda6ce0595c0a74f5934249020c78f121f168
Jhonatan <ojedasnachezjhonatan@gmail.com>
2025-01-30 14:52:05 -0500

    añadiendo mensajes de errores a la vista formulario

--- src/main/java/com/isai/springformularios/controllers/FormController.java
+++ src/main/java/com/isai/springformularios/controllers/FormController.java
@@ -2,12 +2,17 @@ package com.isai.springformularios.controllers;
 
 import com.isai.springformularios.models.Usuario;
 import com.isai.springformularios.service.UsurarioServiceImpl.UsuarioServiceImpl;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 @Controller
 public class FormController {
@@ -16,19 +21,26 @@ public class FormController {
 
     @GetMapping("/form")
     public String form(Model model) {
-        model.addAttribute("tittle", "Resultado Usuarios");
+        model.addAttribute("tittle", "Formulario Usuarios");
         return "form";
     }
 
     @PostMapping("/form")
-    public String procesar(Model model,
-                           @RequestParam String username,
-                           @RequestParam String password,
-                           @RequestParam String email) {
-        Usuario usuario = new Usuario();
-        usuario.setUsername(username);
-        usuario.setPassword(password);
-        usuario.setEmail(email);
+    public String procesar(@Valid Usuario usuario, BindingResult result, Model model) {
+        //validamos
+        if (result.hasErrors()) {
+            //retornamos a la vista
+            Map<String, String> errors = new HashMap<>();
+            result.getFieldErrors()
+                    .forEach(error -> {
+                        errors.put(error.getField(),
+                                "El campo ".concat(error.getField())
+                                        .concat(" ")
+                                        .concat(Objects.requireNonNull(error.getDefaultMessage())));
+                    });
+            model.addAttribute("errors", errors);
+            return "form";
+        }
         usuarioServiceImpl.save(usuario);
         model.addAttribute("tittle", "Resultado Form");
         model.addAttribute("usuario", usuario);
--- src/main/java/com/isai/springformularios/models/Usuario.java
+++ src/main/java/com/isai/springformularios/models/Usuario.java
@@ -1,6 +1,7 @@
 package com.isai.springformularios.models;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,7 +15,10 @@ public class Usuario {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int usuarioId;
+    @NotEmpty
     private String username;
+    @NotEmpty
     private String password;
+    @NotEmpty
     private String email;
 }
