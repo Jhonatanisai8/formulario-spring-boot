93892a8f2dcea131e8ab27ff79d672b93f7ed018
Jhonatan <ojedasnachezjhonatan@gmail.com>
2025-01-29 22:35:50 -0500

    metodos handler para enviar y recibir datos

--- src/main/java/com/isai/springformularios/controllers/FormController.java
+++ src/main/java/com/isai/springformularios/controllers/FormController.java
@@ -4,16 +4,25 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class FormController {
     @GetMapping("/form")
-    public String form(Model model){
+    public String form(Model model) {
+        model.addAttribute("tittle", "Resultado Usuarios");
         return "form";
     }
 
     @PostMapping("/form")
-    public String procesar(Model model){
+    public String procesar(Model model,
+                           @RequestParam String username,
+                           @RequestParam String password,
+                           @RequestParam String email) {
+        model.addAttribute("tittle", "Resultado Form");
+        model.addAttribute("username", username);
+        model.addAttribute("password", password);
+        model.addAttribute("email", email);
         return "resultado";
     }
 }
