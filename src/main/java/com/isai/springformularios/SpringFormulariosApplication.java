ebca0bbc87105a1ad8a07dec791a1a7da25429fc
Jhonatan <ojedasnachezjhonatan@gmail.com>
2025-01-29 22:19:47 -0500

    dependencias necesarias

---
+++ src/main/java/com/isai/springformularios/SpringFormulariosApplication.java
@@ -0,0 +1,13 @@
+package com.isai.springformularios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFormulariosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFormulariosApplication.class, args);
+    }
+
+}
