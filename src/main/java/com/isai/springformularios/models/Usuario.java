b20c7d1fd17aa5fc80c0ddf26d9d2631cb46e297
Jhonatan <ojedasnachezjhonatan@gmail.com>
2025-02-06 11:18:09 -0500

    anotacion size para rangos y email para validar el correo electronico

--- src/main/java/com/isai/springformularios/models/Usuario.java
+++ src/main/java/com/isai/springformularios/models/Usuario.java
@@ -1,7 +1,9 @@
 package com.isai.springformularios.models;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,14 +18,20 @@ public class Usuario {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int usuarioId;
     private String identificadorDni;
-    @NotEmpty
+    @NotEmpty(message = "El nombre no puede ser vacio.")
+    @Size(min = 10, max = 15)
     private String nombre;
     @NotEmpty
+    @Size(min = 10, max = 15)
     private String apellido;
     @NotEmpty
+    @Size(min = 3, max = 8)
     private String username;
     @NotEmpty
+    @Size(min = 8, max = 20)
     private String password;
     @NotEmpty
+    @Email(message = "Correo con formato incorrecto")
+    @Size(min = 10, max = 25)
     private String email;
 }
