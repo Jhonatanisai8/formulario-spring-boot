8ed25140a380251ebeeddd5db5bcaef43e16e888
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-02-09 20:30:39 -0500

    validacion personalizado en el atributo identificador utilizando la anotacion Pattern

--- src/main/java/com/isai/springformularios/models/Usuario.java
+++ src/main/java/com/isai/springformularios/models/Usuario.java
@@ -3,6 +3,7 @@ package com.isai.springformularios.models;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -17,19 +18,27 @@ public class Usuario {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int usuarioId;
+
+    @Pattern(regexp = "[0-9]{2}.[0-9]{3}.[0-9]{3}[-][A-Z]{1}") //patron a seguir
     private String identificadorDni;
+
+
     @NotEmpty
     @Size(min = 10, max = 15)
     private String nombre;
+
     @NotEmpty
     @Size(min = 10, max = 15)
     private String apellido;
+
     @NotEmpty
     @Size(min = 3, max = 8)
     private String username;
+
     @NotEmpty
     @Size(min = 8, max = 20)
     private String password;
+
     @NotEmpty
     @Email(message = "Correo con formato incorrecto")
     @Size(min = 10, max = 25)
