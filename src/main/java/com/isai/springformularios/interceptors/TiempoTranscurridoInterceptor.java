093303b97808ef11475e5a1440e90021f9c00b58
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-03-16 23:08:51 -0500

    clase TiempoTranscurridoInterceptor: funcianalidad al metodo preHandle

--- src/main/java/com/isai/springformularios/interceptors/TiempoTranscurridoInterceptor.java
+++ src/main/java/com/isai/springformularios/interceptors/TiempoTranscurridoInterceptor.java
@@ -2,20 +2,34 @@ package com.isai.springformularios.interceptors;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.Random;
+
 @Component
 public class TiempoTranscurridoInterceptor
         implements HandlerInterceptor {
+
+    private static final Logger logger = LoggerFactory.getLogger(TiempoTranscurridoInterceptor.class);
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        logger.info("TiempoTranscurridoInterceptor.preHandle entrando......");
+        Long tiempoTranscurrido = System.currentTimeMillis();
+        request.setAttribute("tiempoTranscurrido", tiempoTranscurrido);
+        Random rand = new Random();
+        Integer demora = rand.nextInt(500); // 0- 499
+        Thread.sleep(demora);
         return true;
     }
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        logger.info("TiempoTranscurridoInterceptor.postHandle saliendo......");
 
     }
 }
