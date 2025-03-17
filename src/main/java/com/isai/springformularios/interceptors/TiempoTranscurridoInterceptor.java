554018d8e600ab33b422fead947e54861bddb287
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-03-16 23:41:46 -0500

    verificando que un Object handler sea una instancia de HandlerMethod

--- src/main/java/com/isai/springformularios/interceptors/TiempoTranscurridoInterceptor.java
+++ src/main/java/com/isai/springformularios/interceptors/TiempoTranscurridoInterceptor.java
@@ -5,6 +5,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -18,7 +19,14 @@ public class TiempoTranscurridoInterceptor
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        if (handler instanceof HandlerMethod) {
+            HandlerMethod handlerMethod = (HandlerMethod) handler;
+            logger.info("Es un metodo del controlador: " + handlerMethod.getMethod().getName());
+        }
+
         logger.info("TiempoTranscurridoInterceptor.preHandle entrando......");
+        logger.info("Interceptando: " + handler);
         Long tiempoInicio = System.currentTimeMillis();
         request.setAttribute("tiempoInicio", tiempoInicio);
         Random rand = new Random();
@@ -32,7 +40,7 @@ public class TiempoTranscurridoInterceptor
         long tiempoFin = System.currentTimeMillis();
         long tiempoInicio = (Long) request.getAttribute("tiempoInicio");
         long tiempoTranscurrido = tiempoFin - tiempoInicio;
-        if (modelAndView != null) {
+        if (handler instanceof HandlerMethod && modelAndView != null) {
             modelAndView.addObject("tiempoTranscurrido", tiempoTranscurrido);
         }
         logger.info("Tiempo Transcurrido: " + tiempoTranscurrido + " milisegundos");
